85fd745632fb9bb06e07b84e3b2d20011684a343
Travis Henderson <tjh@Traviss-MacBook-Air.local>
2025-05-25 18:15:29 +0900

    I think done with cpu

--- internal/metrics/cpu/cpu.go
+++ internal/metrics/cpu/cpu.go
@@ -5,51 +5,61 @@ import (
 	"time"
 
 	syscpu "github.com/shirou/gopsutil/v4/cpu"
+	sysload "github.com/shirou/gopsutil/v4/load"
 )
 
 type CpuMetric struct {
-	CpuUsages []CpuUsage
-	LoadAvg   float64
-	TimeStamp time.Time
+	Usage        []float64 // CPU usage as a percentage over a given time interval, each entry represents a core.
+	TimeInterval float64   // The time interval for which usage percentage of the cpu is taken from.
+	LoadAvg1     float64   // Average system load (number of processes running/waiting) over the past 1 minute.
+	LoadAvg5     float64   // Average system load (number of processes running/waiting) over the past 5 minutes.
+	LoadAvg15    float64   // Average system load (number of processes running/waiting) over the past 15 minutes.
+	TimeStamp    time.Time // Time the measurement was taken.
 }
 
-type CpuUsage struct {
-	CpuName string
-	Usage   float64
-}
+type Option func(*float64)
 
-func getCpuMetrics() (CpuMetric, error) {
-	time := time.Now()
-
-	percentages, err := syscpu.Percent(5, true)
-	if err != nil {
-		return CpuMetric{}, err
-	}
-	var cpuUsages []CpuUsage
-	var name string
-	for index, percent := range percentages {
-		if index != 0 {
-			name = fmt.Sprintf("cpu%d", index)
-		} else {
-			name = "cpuOverall"
+func WithSeconds(seconds float64) Option {
+	return func(input *float64) {
+		if seconds < 0 || seconds > 180 {
+			// TODO: log
+			return
 		}
-		cpuUsages = append(cpuUsages, CpuUsage{
-			CpuName: name,
-			Usage:   percent,
-		})
+		*input = seconds
+		/// This seems unneccessarily comples just for an int...
 	}
-	loadAvg, err := syscpu.LoadAvg()
-	return CpuMetric{
-		CpuUsages: cpuUsages,
+}
 
-		TimeStamp: time,
+func getCpuMetrics(options ...Option) (CpuMetric, error) {
+	seconds := 1.0
+	for _, opt := range options {
+		opt(&seconds)
+	}
+
+	percentages, err := syscpu.Percent(time.Duration(seconds)*time.Second, true)
+	if err != nil {
+		return CpuMetric{}, fmt.Errorf("Error getting CPU usage: %v", err)
+	}
+
+	loadAvg, err := sysload.Avg()
+	if err != nil {
+		return CpuMetric{}, fmt.Errorf("Error in getting load average: %v", err)
+	}
+	return CpuMetric{
+		Usage:        percentages,
+		TimeInterval: seconds,
+		LoadAvg1:     loadAvg.Load1,
+		LoadAvg5:     loadAvg.Load5,
+		LoadAvg15:    loadAvg.Load15,
+		TimeStamp:    time.Now(),
 	}, nil
 }
 
-func getCpuUsage() ([]float64, error) {
-	percentages, err := syscpu.Percent(5, true)
-	if err != nil {
-		return []float64{}, err
+func (cm CpuMetric) String() string {
+	retval := "Usage: "
+	for _, percentage := range cm.Usage {
+		retval += fmt.Sprintf("%.2f ", percentage)
 	}
-	return percentages, nil
+	retval += fmt.Sprintf("\nTimeInterval: %.2f\nLoadAvg1: %.2f\nLoadAvg5: %.2f\nLoadAvg15: %.2f\nTimeStamp: %v", cm.TimeInterval, cm.LoadAvg1, cm.LoadAvg5, cm.LoadAvg15, cm.TimeStamp)
+	return retval
 }
--- internal/metrics/cpu/cpu_test.go
+++ internal/metrics/cpu/cpu_test.go
@@ -1,13 +1,38 @@
 package cpu
 
 import (
+	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestGetCpuUsage(t *testing.T) {
-	got, err := getCpuUsage()
+func TestGetCpuMetrics(t *testing.T) {
+	got, err := getCpuMetrics(WithSeconds(0.2))
 	require.Nil(t, err)
-	t.Log(got)
+	assert.Equal(t, 0.2, got.TimeInterval)
+	assert.False(t, got.TimeStamp.IsZero())
+	assert.NotZero(t, len(got.Usage))
+	assert.Greater(t, got.LoadAvg1, 0.0)
+	assert.Greater(t, got.LoadAvg5, 0.0)
+	assert.Greater(t, got.LoadAvg15, 0.0)
+}
+
+func TestString(t *testing.T) {
+	input := CpuMetric{
+		Usage:        []float64{1, 2, 3},
+		TimeInterval: 0.3,
+		LoadAvg1:     0.1,
+		LoadAvg5:     0.2,
+		LoadAvg15:    0.3,
+	}
+	expected := `Usage: 1.00 2.00 3.00 
+        TimeInterval: 0.30
+        LoadAvg1: 0.10
+        LoadAvg5: 0.20
+        LoadAvg15: 0.30
+        TimeStamp: 0001-01-01 00:00:00 +0000 UTC`
+
+	assert.Equal(t, strings.Fields(expected), strings.Fields(input.String()))
 }
