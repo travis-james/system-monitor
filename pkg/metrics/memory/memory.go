b46833621dd4aed4c45d1595a2e870b2978ccb16
thinkpad <me@thinkpad.com>
2025-05-31 16:44:50 +0900

    memory functionality done

---
+++ pkg/metrics/memory/memory.go
@@ -0,0 +1,25 @@
+package memory
+
+import gopsutilMem "github.com/shirou/gopsutil/v4/mem"
+
+type MemoryMetric struct {
+	UsedMemory      uint64
+	AvailableMemory uint64
+}
+
+func MeasureMemoryMetrics() (MemoryMetric, error) {
+	return measureMemoryMetrics(gopsutilMem.VirtualMemory)
+}
+
+type virtualMemoryFunc func() (*gopsutilMem.VirtualMemoryStat, error)
+
+func measureMemoryMetrics(getVirtualMemory virtualMemoryFunc) (MemoryMetric, error) {
+	memStats, err := getVirtualMemory()
+	if err != nil {
+		return MemoryMetric{}, err
+	}
+	return MemoryMetric{
+		UsedMemory:      memStats.Used,
+		AvailableMemory: memStats.Available,
+	}, nil
+}
---
+++ pkg/metrics/memory/memory_test.go
@@ -0,0 +1,37 @@
+package memory
+
+import (
+	"errors"
+	"testing"
+
+	gopsutilMem "github.com/shirou/gopsutil/v4/mem"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// Mock function for testing
+func mockVirtualMemory(stats *gopsutilMem.VirtualMemoryStat, err error) virtualMemoryFunc {
+	return func() (*gopsutilMem.VirtualMemoryStat, error) {
+		return stats, err
+	}
+}
+
+func TestMeasureMemoryMetrics_ValidStats(t *testing.T) {
+	var (
+		used      uint64 = 2048
+		available uint64 = 4096
+		mockStats        = &gopsutilMem.VirtualMemoryStat{
+			Used:      used,
+			Available: available,
+		}
+	)
+	got, err := measureMemoryMetrics(mockVirtualMemory(mockStats, nil))
+	require.Nil(t, err)
+	assert.Equal(t, used, got.UsedMemory)
+	assert.Equal(t, got, got.AvailableMemory)
+}
+
+func TestMeasureMemoryMetrics_ErrorCase(t *testing.T) {
+	_, err := measureMemoryMetrics(mockVirtualMemory(nil, errors.New("failed to get memory stats")))
+	assert.NotNil(t, err)
+}
